24a3a9a2641373c48953084f2790b707e7221c0c
Hunter White <hpw6813@gmail.com>
2025-11-08 14:14:30 -0500

    setting up the component and app

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import logo from '../logo.svg'
+
+import Header from './Header'
+
+function App() {
+    return (
+        <div>
+            <Header pageTitle="PrayerPost with JWT" logoSrc = {logo}></Header>
+        </div>
+    )
+}
+
+export default App;
