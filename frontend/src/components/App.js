be1e47a71cdc194098cfbedaa45d74c7f9b28b84
Hunter White <hpw6813@gmail.com>
2025-11-08 18:00:44 -0500

    prayer application

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,11 +2,15 @@ import './App.css'
 import logo from '../logo.svg'
 
 import Header from './Header'
+import AppContent from './AppContent';
 
 function App() {
     return (
         <div>
             <Header pageTitle="PrayerPost with JWT" logoSrc = {logo}></Header>
+            <div>
+            <AppContent></AppContent>
+            </div>
         </div>
     )
 }
---
+++ frontend/src/components/WelcomeContent.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+
+
+export default class WelcomeContent extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Welcome Back!</h1>
+                <h2>Lift up others through Faith!</h2>
+                <p>Login or Register in order to access more Content!</p>
+            </div>
+        )
+    }
+}
